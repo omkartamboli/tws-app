eaebeada422a5fa4d540ee340bf9aee22a7d7f51
Omkar Tamboli <omkar.tamboli@acorel.nl>
2022-06-27 14:01:05 +0100

    fix props issue

--- src/test/resources/dbSchema.sql
+++ src/test/resources/dbSchema.sql
@@ -1,15 +1,5 @@
--- create database cpp;
--- create user 'cpp'@'%' identified by 'cpp';
--- grant all privileges on cpp.* to 'cpp'@'%' with grant option;
+create database tradingApp;
+create user 'tradingApp'@'localhost' identified by 'tradingApp';
+grant all privileges on tradingApp.* to 'tradingApp'@'localhost' with grant option;
 
 
--- drop database cpp;
-
-
-
-create database tradingAppcontract_entity;
-create user 'tradingApp'@'%' identified by 'tradingApp';
-grant all privileges on tradingApp.* to 'tradingApp'@'%' with grant option;
-
-
--- drop database cpp;
